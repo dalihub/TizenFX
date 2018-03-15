cf0b8853b8b3b411512c908cf6dc8bbd8d383fc3
Agnelo Vaz <agnelo.vaz@samsung.com>
2018-03-15 12:23:15 +0000

    Layouting API
    Change-Id: I8ffa2b8497ba367f29550d76ba03e21c43dc161c

--- src/Tizen.NUI/src/internal/ManualPINVOKE.cs
+++ src/Tizen.NUI/src/internal/ManualPINVOKE.cs
@@ -1250,6 +1250,13 @@ namespace Tizen.NUI
         [global::System.Runtime.InteropServices.DllImport("libdali-csharp-binder.so", EntryPoint="CSharp_Dali_ViewLayoutDirectionSignal_Emit")]
         public static extern void ViewLayoutDirectionChangedSignal_Emit(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
 
+        [global::System.Runtime.InteropServices.DllImport("libdali-csharp-binder.so", EntryPoint="CSharp_Dali_View_SetLayout")]
+        public static extern void View_SetLayout(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2 );
+
+        [global::System.Runtime.InteropServices.DllImport("libdali-csharp-binder.so", EntryPoint="CSharp_Dali_View_GetLayout")]
+        public static extern global::System.IntPtr View_GetLayout(global::System.Runtime.InteropServices.HandleRef jarg1 );
+
+
         [global::System.Runtime.InteropServices.DllImport("libdali-csharp-binder.so", EntryPoint="CSharp_Dali_new_ViewLayoutDirectionSignal")]
         public static extern global::System.IntPtr new_ViewLayoutDirectionChangedSignal();
 
