6ba81855185f295410d22e35c649331fcc3e59c9
dongsug.song <dongsug.song@samsung.com>
2018-07-18 20:24:03 +0900

    Make Layout APIs as internal ones (will be changed as public APIs later)
    Change-Id: Ia64e9216d10ecc986e0e977f2e0633a03813faec
    Signed-off-by: dongsug.song <dongsug.song@samsung.com>

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -3418,10 +3418,7 @@ namespace Tizen.NUI.BaseComponents
         /// </summary>
         /// <remarks>
         /// </remarks>
-        /// <since_tizen> 5 </since_tizen>
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public LayoutItem Layout
+        internal LayoutItem Layout
         {
             get
             {
@@ -5398,9 +5395,10 @@ namespace Tizen.NUI.BaseComponents
 
         internal readonly MergedStyle _mergedStyle;
 
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public int WidthSpecificationFixed
+        /// <summary>
+        /// Child property to specify desired width
+        /// </summary>
+        internal int LayoutWidthSpecificationFixed
         {
             get
             {
@@ -5416,10 +5414,11 @@ namespace Tizen.NUI.BaseComponents
                 SetProperty(LayoutItemWrapper.ChildProperty.WIDTH_SPECIFICATION, new Tizen.NUI.PropertyValue(value));
             }
         }
-        
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public int HeightSpecificationFixed
+
+        /// <summary>
+        /// Child property to specify desired height
+        /// </summary>
+        internal int LayoutHeightSpecificationFixed
         {
             get
             {
@@ -5435,10 +5434,11 @@ namespace Tizen.NUI.BaseComponents
                 SetProperty(LayoutItemWrapper.ChildProperty.HEIGHT_SPECIFICATION, new Tizen.NUI.PropertyValue(value));
             }
         }
-        
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public ChildLayoutData WidthSpecification
+
+        /// <summary>
+        /// Child property to specify desired width, use MatchParent/WrapContent)
+        /// </summary>
+        internal ChildLayoutData LayoutWidthSpecification
         {
             get
             {
@@ -5454,10 +5454,11 @@ namespace Tizen.NUI.BaseComponents
                 SetProperty(LayoutItemWrapper.ChildProperty.WIDTH_SPECIFICATION, new Tizen.NUI.PropertyValue((int)value));
             }
         }
-        
-        /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public ChildLayoutData HeightSpecification
+
+        /// <summary>
+        /// Child property to specify desired height, use MatchParent/WrapContent)
+        /// </summary>
+        internal ChildLayoutData LayoutHeightSpecification
         {
             get
             {
@@ -5474,11 +5475,7 @@ namespace Tizen.NUI.BaseComponents
             }
         }
 
-
     }
-
-
-
 
     /// <summary>
     /// The View layout Direction type.
@@ -5498,11 +5495,15 @@ namespace Tizen.NUI.BaseComponents
         RTL
     }
 
-    /// This will be public opened in tizen_5.0 after ACR done. Before ACR, need to be hidden as inhouse API.
-    [EditorBrowsable(EditorBrowsableState.Never)]
-    public enum ChildLayoutData
+    internal enum ChildLayoutData
     {
+        /// <summary>
+        /// Constant which indicates child size should match parent size
+        /// </summary>
         MatchParent = -1,
+        /// <summary>
+        /// Constant which indicates parent should take the smallest size possible to wrap it's children with their desired size
+        /// </summary>
         WrapContent = -2,
     }
 
