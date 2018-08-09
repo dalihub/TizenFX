318d90b37a4134d94c2c92f3d9f42cda25b4c0e3
adun <xiaohui.fang@samsung.com>
2018-08-09 14:49:02 +0800

    [NUI]Add protect codes in API View::Add
    1. Add protect codes, so that if the child is null, this API won't don anything.
    
    Change-Id: Icfd7e2cbb08b99ed962660edb88e8797bad2b654

--- src/Tizen.NUI/src/public/BaseComponents/View.cs
+++ src/Tizen.NUI/src/public/BaseComponents/View.cs
@@ -1275,6 +1275,11 @@ namespace Tizen.NUI.BaseComponents
         /// <since_tizen> 4 </since_tizen>
         public override void Add(View child)
         {
+			if (null == child)
+			{
+				return;
+			}
+
             Container oldParent = child.Parent;
             if (oldParent != this)
             {
