14e6275bb035e85b1c506706411b598a2b769c76
xb.teng <xb.teng@samsung.com>
2018-11-13 13:18:53 +0800

    [NUI] Add reverse support for parse
    Change-Id: Ia6f55508e34cf5adbf23d0f9acd4f69da7b6d3c5

--- src/Tizen.NUI/src/internal/XamlBinding/ExtentsTypeConverter.cs
+++ src/Tizen.NUI/src/internal/XamlBinding/ExtentsTypeConverter.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
 using System.Reflection;
-
+using System.Globalization;
 using Tizen.NUI;
 
 namespace Tizen.NUI.Binding
@@ -15,7 +15,10 @@ namespace Tizen.NUI.Binding
                 string[] parts = value.Split(',');
                 if (parts.Length == 4)
                 {
-                    return new Extents(ushort.Parse(parts[0].Trim()), ushort.Parse(parts[1].Trim()), ushort.Parse(parts[2].Trim()), ushort.Parse(parts[3].Trim()));
+                    return new Extents(ushort.Parse(parts[0].Trim(), CultureInfo.InvariantCulture),
+                        ushort.Parse(parts[1].Trim(), CultureInfo.InvariantCulture),
+                        ushort.Parse(parts[2].Trim(), CultureInfo.InvariantCulture),
+                        ushort.Parse(parts[3].Trim(), CultureInfo.InvariantCulture));
                 }
             }
 
