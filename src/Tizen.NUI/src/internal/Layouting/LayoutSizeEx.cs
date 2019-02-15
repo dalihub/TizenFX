d6bd64b351a0f28b5fd7213f4ddc527b791aa76e
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-02-15 15:09:09 +0000

    Add LayoutSizeEx class
    Change-Id: I15738a2fc8c40da90ebe89888cf1779b9124b98e

---
+++ src/Tizen.NUI/src/internal/Layouting/LayoutSizeEx.cs
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2019 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+using System.ComponentModel;
+using Tizen.NUI.BaseComponents;
+
+namespace Tizen.NUI
+{
+    /// <summary>
+    /// [Draft] This class represents a layout size (width and height)
+    /// </summary>
+    internal class LayoutSizeEx
+    {
+        private int _width;
+        private int _height;
+
+        /// <summary>
+        /// [Draft] Constructor from width and height
+        /// </summary>
+        /// <param name="width">Int to initialize with.</param>
+        /// <param name="height">Int to initialize with.</param>
+        public LayoutSizeEx(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+
+        /// <summary>
+        /// [Draft] Check if LayoutSize objects are equal
+        /// </summary>
+        /// <param name="target">The LayoutSize object to compare against.</param>
+        /// <returns>true if equal LayoutSize objects, else false.</returns>
+        public bool IsEqualTo(LayoutSizeEx target)
+        {
+            if (_width == target._width && _height == target._height)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// [Draft] Get the width value of this layout
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+        }
+
+        /// <summary>
+        /// [Draft] Get the height value of this layout
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+        }
+    }
+}
