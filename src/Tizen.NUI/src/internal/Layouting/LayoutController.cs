4fd4f069afba8558824098e6bf5ce8ac5a0c3412
Agnelo Vaz <agnelo.vaz@samsung.com>
2019-02-15 15:10:30 +0000

    LayoutController View updates
    Change-Id: Ie87b6c90eef207711568d4c56f496898f089d099

--- src/Tizen.NUI/src/internal/Layouting/LayoutController.cs
+++ src/Tizen.NUI/src/internal/Layouting/LayoutController.cs
@@ -38,6 +38,7 @@ namespace Tizen.NUI
         //A Flat to check if it is already disposed.
         protected bool disposed = false;
 
+        View root;
 
         /// <summary>
         /// Constructs a LayoutController which controls the measuring and layouting.<br />
@@ -116,6 +117,40 @@ namespace Tizen.NUI
         private void Process(int id)
         {
             NUILog.Debug("layoutController Process id:" + id );
+            // root, currently is the Window, needs to be View or derive from a class that
+            // is implemented by View, Layer and Window.
+
+            // Start at root with it's widthSpec and heightSpec
+            MeasureHierarchy( root, root.MeasureSpecificationWidth, root.MeasureSpecificationHeight );
+
+            // Start at root with it's widthSpec and heightSpec
+            PerformLayout( root, new LayoutLengthEx(0), new LayoutLengthEx(0),
+                           root.MeasureSpecificationWidth.Size, root.MeasureSpecificationHeight.Size );
+        }
+
+        /// <summary>
+        /// Starts measuring the tree, starting from the root layout.
+        /// </summary>
+        private void MeasureHierarchy(View root, MeasureSpec widthSpec, MeasureSpec heightSpec)
+        {
+            // Does this View have a layout?
+            // Yes - measure the layout. It will call this method again for each of it's children.
+            // No -  reached leaf or no layouts set
+            //
+            // If in a leaf View with no layout, it's natural size is bubbled back up.
+
+            if(root.LayoutEx != null)
+            {
+                root.LayoutEx.Measure(widthSpec, heightSpec);
+            }
+        }
+
+        private void PerformLayout(View root, LayoutLengthEx left, LayoutLengthEx top, LayoutLengthEx right, LayoutLengthEx bottom)
+        {
+            if(root.LayoutEx != null)
+            {
+                root.LayoutEx.Layout(left, top, right, bottom);
+            }
         }
 
     } // class LayoutController
