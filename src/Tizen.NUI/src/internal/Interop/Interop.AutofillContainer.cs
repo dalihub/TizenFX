35abc7af793597b7a813fd63a9276160d159fb4b
Seoyeon Kim <seoyeon2.kim@samsung.com>
2019-05-16 14:09:58 +0900

    [NUI] Add AutofillContainer and autofill implementation
    Change-Id: Ifc219498c53577e6560b84a8065c22c3b03e2859
    Signed-off-by: Seoyeon Kim <seoyeon2.kim@samsung.com>

---
+++ src/Tizen.NUI/src/internal/Interop/Interop.AutofillContainer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class AutofillContainer
+        {
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_SWIGUpcast")]
+            public static extern global::System.IntPtr AutofillContainer_SWIGUpcast(global::System.IntPtr jarg1);
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_AutofillContainer_New")]
+            public static extern global::System.IntPtr AutofillContainer_New(string jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_AutofillContainer__SWIG_1")]
+            public static extern global::System.IntPtr new_AutofillContainer__SWIG_1(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_AutofillContainer__SWIG_0")]
+            public static extern global::System.IntPtr new_AutofillContainer__SWIG_0();
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_Assign")]
+            public static extern global::System.IntPtr AutofillContainer_Assign(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_AutofillContainer")]
+            public static extern void delete_AutofillContainer(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_DownCast")]
+            public static extern global::System.IntPtr AutofillContainer_DownCast(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_AutofillContainer_AddAutofillItem")]
+            public static extern void AutofillContainer_AddAutofillItem(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_GetAutofillGroup")]
+            public static extern global::System.IntPtr AutofillContainer_GetAutofillGroup(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_AutofillServiceShownSignal")]
+            public static extern global::System.IntPtr AutofillContainer_AutofillServiceShownSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_AutofillFillRequestSignal")]
+            public static extern global::System.IntPtr AutofillContainer_AutofillFillRequestSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_AutofillContainer_AutofillListShownSignal")]
+            public static extern global::System.IntPtr AutofillContainer_AutofillListShownSignal(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+        }
+    }
+}
---
+++ src/Tizen.NUI/src/internal/Interop/Interop.ListSignalType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tizen.NUI
+{
+    internal static partial class Interop
+    {
+        internal static partial class ListSignalType
+        {
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListSignalType_Empty")]
+            public static extern bool ListSignalType_Empty(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListSignalType_GetConnectionCount")]
+            public static extern uint ListSignalType_GetConnectionCount(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListSignalType_Connect")]
+            public static extern void ListSignalType_Connect(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListSignalType_Disconnect")]
+            public static extern void ListSignalType_Disconnect(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_ListSignalType_Emit")]
+            public static extern void ListSignalType_Emit(global::System.Runtime.InteropServices.HandleRef jarg1, global::System.Runtime.InteropServices.HandleRef jarg2);
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_new_ListSignalType")]
+            public static extern global::System.IntPtr new_ListSignalType();
+
+
+            [global::System.Runtime.InteropServices.DllImport(NDalicPINVOKE.Lib, EntryPoint = "CSharp_Dali_delete_ListSignalType")]
+            public static extern void delete_ListSignalType(global::System.Runtime.InteropServices.HandleRef jarg1);
+
+        }
+    }
+}
